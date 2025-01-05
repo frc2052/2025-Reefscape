39730dbdda6896e5787fc26a0fa5b2bbd04c6568
Caleb Fynewever <caleb.fynewever@gmail.com>
2025-01-05 13:06:41 -0600

    Changing permission of gradlew

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-01-04 21:35:33 EST";
-  public static final long BUILD_UNIX_TIME = 1736044533599L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "e9709a9dfd155b0ec9a4e345877c1310b9549c94";
+  public static final String GIT_DATE = "2025-01-05 13:54:37 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-05 13:59:58 EST";
+  public static final long BUILD_UNIX_TIME = 1736103598675L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
