c58fff38e1e5544de12f3fac4c0027c548f9d1f8
MasonLilley <102882288+MasonLilley@users.noreply.github.com>
2025-01-09 21:57:19 -0600

    remove redunant "SR"

--- src/main/java/frc/robot/auto/modes/StartRight/AutoE2D4C4.java
+++ src/main/java/frc/robot/auto/modes/StartRight/AutoE2D4C4.java
@@ -3,12 +3,12 @@ package frc.robot.auto.modes.StartRight;
 import com.pathplanner.lib.path.PathPlannerPath;
 import frc.robot.auto.common.AutoBase;
 
-public class AutoSRE2D4C4 extends AutoBase {
+public class AutoE2D4C4 extends AutoBase {
   // Start Left Equivalent: AutoJ2K4L4
 
   private static final PathPlannerPath startingPath = Paths.SR_E2;
 
-  public AutoSRE2D4C4() {
+  public AutoE2D4C4() {
     super(startingPath.getStartingHolonomicPose());
   }
 
