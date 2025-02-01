20659ff04eb7a2dab8e7471f38fd64a3e4cd789a
Caleb Fynewever <104877856+caleb-fynewever@users.noreply.github.com>
2025-02-01 15:55:13 -0600

    miiiiight need further tuning but basically done, very consistent

--- src/main/java/com/team2052/lib/planners/AutoAlignPlanner.java
+++ src/main/java/com/team2052/lib/planners/AutoAlignPlanner.java
@@ -16,9 +16,9 @@ public class AutoAlignPlanner {
 
   public AutoAlignPlanner() {
     startTime = OptionalDouble.of(Timer.getFPGATimestamp());
-    xController = new PIDFFController(2.5, 0.0, 0, 0.0, 0, 0.0, 0.0, 0.0);
-    yController = new PIDFFController(3.5, 0.0, 0, 0.1, 0, 0.0, 0.0, 0.0);
-    thetaController = new PIDFFController(1.0, 0.0, 0.1, 0.3, 0.0, 0.0, 3.0, 2.0);
+    xController = new PIDFFController(3, 0.0, 0, 0.0, 0, 0.0, 0.0, 0.0);
+    yController = new PIDFFController(4, 0.0, 0, 0.1, 0, 0.0, 0.0, 0.0);
+    thetaController = new PIDFFController(3.0, 0.0, 0.5, 0.3, 0.0, 0.0, 3.0, 2.0);
 
     xController.setTolerance(0.08, 0.05);
     yController.setTolerance(0.02, 0.05);
--- src/main/java/frc/robot/commands/drive/AlignWithReefCommand.java
+++ src/main/java/frc/robot/commands/drive/AlignWithReefCommand.java
@@ -120,9 +120,9 @@ public class AlignWithReefCommand extends DefaultDriveCommand {
   }
 
   public enum AlignLocation { // provides an offset from the april tag
-    LEFT(new Transform2d(0.5, 0.5, new Rotation2d(0))),
+    LEFT(new Transform2d(0.5, 0.25, new Rotation2d(0))),
     MIDDLE(new Transform2d(0.5, 0.0, new Rotation2d(0))),
-    RIGHT(new Transform2d(0.5, -0.5, new Rotation2d(0)));
+    RIGHT(new Transform2d(0.5, -0.25, new Rotation2d(0)));
 
     public Transform2d transform;
 
